45fbe071a38a0f3f92b04ffc916dfd4f3dbf5d84
aanyakun <aanya.kungwani@gmail.com>
2025-09-24 17:24:13 +0000

    changes!!

--- main.js
+++ main.js
@@ -12,6 +12,15 @@ oscillator.start();
 gainNode.gain.value = 0;
 
 
+notes = new Map();
+notes.set("C", 261.6);
+notes.set("D", 293.7);
+notes.set("E", 329.6);
+notes.set("F", 349.2);
+notes.set("G", 392.0);
+notes.set("A", 440);
+notes.set("B", 493.9);
+
 function frequency(pitch) {
 gainNode.gain.setValueAtTime(100, audioCTX.currentTime);
 oscillator.frequency.setValueAtTime(pitch, audioCTX.currentTime);
@@ -22,5 +31,6 @@ gainNode.gain.setValueAtTime(0, audioCTX.currentTime + 1);
 function handle() {
     audioCTX.resume();
     gainNode.gain.value = 0;
-    frequency(input.value);
+   var usersnotes = String(input.value)
+   frequency(notes.get(usersnotes));
 }
