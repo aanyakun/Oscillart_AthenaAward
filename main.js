deb13f3b8bda34879be4991aa0dbaa6201caf1ef
aanyakun <aanya.kungwani@gmail.com>
2025-09-27 15:38:02 +0000

    hi

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var width = ctx.canvas.width;
 var height = ctx.canvas.height;
 var amplitude = 54;
 var interval = null;
+var reset = false;
+var timepernote = 0;
+var length = 0;
 
 
 const input = document.getElementById('input');
@@ -35,15 +38,33 @@ function frequency(pitch) {
     currentPitch = pitch;
 gainNode.gain.setValueAtTime(100, audioCTX.currentTime);
 oscillator.frequency.setValueAtTime(pitch, audioCTX.currentTime);
-gainNode.gain.setValueAtTime(0, audioCTX.currentTime + 1);
+gainNode.gain.setValueAtTime(0, audioCTX.currentTime + timepernote/1000) - 0.1;
 }
 
 
 function handle() {
+    reset = true;
+    var usersnotes = String(input.value);
+    length = usersnotes.length;
+    timepernote = (6000/ length);
+    var noteslist = [];
+    for (i=0; i < usersnotes.length; i++) {
+    noteslist.push(notes.get(usersnotes.charAt(i)));
+    }
+    let j = 0;
+    repeat = setInterval(() => {
+        if (j < noteslist.length) {
+            frequency(parseInt(noteslist[j]));
+            drawWave();
+            j++;
+        } else {
+            clearInterval(repeat);
+        }
+
+        }, timepernote);
+    
     audioCTX.resume();
     gainNode.gain.value = 0;
-   var usersnotes = String(input.value);
-   frequency(notes.get(usersnotes));
    drawWave();
 }
 
@@ -51,21 +72,25 @@ var counter = 0;
 
 function line() {
     let freq = currentPitch / 10000;
-    y= height/2 + amplitude * Math.sin(x*2*Math.PI*freq);
+    y= height/2 + amplitude * Math.sin(x*2*Math.PI*freq * (0.5 *length));
     ctx.lineTo(x,y);
     ctx.stroke();
     x = x + 1;
     counter++;
-    if (counter > 50) {
+    if (counter > (timepernote/20)) {
         clearInterval(interval);
     }
 }
 
-function drawWave(){
+function drawWave() {
+    clearInterval(interval);
+    if (reset) {
     ctx.clearRect(0,0,width, height);
     x = 0;
     y = height/2;
     ctx.beginPath();
+    }
+    reset = false;
     ctx.moveTo(x,y);
     counter = 0;
     interval = setInterval(line, 20);
